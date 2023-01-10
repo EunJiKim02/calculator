354d293130447d04b9025bcc250394c5ff3fa706
mobuktodae <now0104@naver.com>
2023-01-10 15:49:49 +0900

    kafka producer와 postgresql 연결

--- getdata.py
+++ getdata.py
@@ -1,6 +1,7 @@
 import json
 
 from kafka import KafkaConsumer
+import psycopg2
 
 KAFKA_TOPIC = 'input_data'
 
@@ -9,10 +10,39 @@ consumer = KafkaConsumer(
   bootstrap_servers="127.0.0.1:9092"
 )
 
+hostname = 'localhost'
+database = 'Calculator'
+username = 'admin'
+pwd = 'mobuk'
+port_id = 5432
 
-print("start listening")
 
-while True:
+try:
+  conn = psycopg2.connect(
+        host=hostname,
+        password=pwd,
+        port=port_id,
+        user=username,
+        dbname=database 
+  )
+  
+  cur = conn.cursor()
+
+  create_script = '''CREATE TABLE IF NOT EXISTS expression(
+  id INT NOT NULL,
+  lh INT NOT NULL,
+  rh INT NOT NULL,
+  op TEXT NOT NULL,
+  result INT NOT NULL,
+  create_at TIMESTAMPTZ DEFAULT now()
+  );'''
+
+  cur.execute(create_script)
+  conn.commit()
+
+
+  print("start listening")
+
   for message in consumer:
     print("get data")
     consumed_message = json.loads(message.value.decode())
@@ -35,7 +65,23 @@ while True:
     "id": ex_id,
     "rh": ex_rh,
     "lh": ex_lh,
-    "op": "add",
+    "op": ex_op,
     "result": result
     }
+    if(ex_lh == 0 and ex_rh == 0):
+      break
+
+    insert_script = ''' INSERT INTO expression (id, lh, rh, result, op) 
+    VALUES (%d, %d, %d, %d, '%s')'''%(ex_id, ex_lh, ex_rh, result, ex_op)
+
+    cur.execute(insert_script)
+    conn.commit()
+
     print(f"{ex_id} >> {ex_lh} {ex_op} {ex_rh} = {result}")
+
+
+
+  cur.close()
+  conn.close()
+except Exception as e:
+  print(e)
--- insertdata.py
+++ insertdata.py
@@ -13,8 +13,7 @@ producer = KafkaProducer(bootstrap_servers='localhost:9092')
 i = 0
 while(True):
   lh, rh = map(int, input("lh, rh 순서대로 입력 (0 0은 종료) >>> ").split())
-  if(lh == 0 and rh == 0):
-    break
+
   op = input("연산 (add sub mul div) >> ")
   data = {
     "id": i,
@@ -25,5 +24,7 @@ while(True):
   i = i + 1
   producer.send(KAFKA_TOPIC, 
     json.dumps(data).encode("utf-8"))
+  if(lh == 0 and rh == 0):
+    break
   print(f"Done Sending..{i}")
   time.sleep(1)
